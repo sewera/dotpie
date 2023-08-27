26d189a2d7d5b2ab59059c9512cb4d8eca15d823
Blazej Sewera <code@sewera.dev>
2023-08-27 23:27:23 +0200

    fix: remove wallsch from i3 scripts

--- n-lx-i3/i3/startup.sh
+++ n-lx-i3/i3/startup.sh
@@ -5,7 +5,7 @@ xrandr \
   --output $(cat ~/.config/i3/monitor-left.txt) --auto --rotate normal --left-of $(cat ~/.config/i3/monitor-center.txt) \
   --output $(cat ~/.config/i3/monitor-right.txt) --auto --rotate normal --right-of $(cat ~/.config/i3/monitor-center.txt)
 
-wallschctl change
+nitrogen --set-zoom-fill /home/jazz/Pictures/Wallpapers/ultra_multihead/desktop/night/night_wh_minecraft_city_lights_shaders.jpg
 
 lockscreen -n /home/jazz/Pictures/Wallpapers/ultra_multihead/desktop/night/night_wh_earth_full.jpg &
 
